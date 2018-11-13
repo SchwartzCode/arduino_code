755d39d2ae0c522cf667e092974a4092ae61558a
SchwartzCode <jonbs98@gmail.com>
2018-11-13 12:16:42 +0000

    Changed simple thermistor reader code

--- MechE_Design_2/thermistor_simple.ino
+++ MechE_Design_2/thermistor_simple.ino
@@ -1,27 +1,44 @@
-//this code was obtained from: https://learn.adafruit.com/thermistor/using-a-thermistor
+#define SERIESRESISTOR 100000    //set resistor value as constant
+#define THERMISTORPIN A0         //set pin value that will read in data
 
-SERIESRESISTOR 10000    
- 
-// What pin to connect the sensor to
-#define THERMISTORPIN A0 
- 
-void setup(void) {
+#define THERMISTORNOMINAL 120000 //resistance of thermistor at room temperature
+#define TEMPERATURENOMINAL 25    //actual temperature used for "room temperature"
+#define BCOEFFICIENT 2950        //beta coefficient of the resistor
+
+void setup() {
   Serial.begin(9600);
+  
+
 }
- 
-void loop(void) {
-  float reading;
- 
-  reading = analogRead(THERMISTORPIN);
- 
-  Serial.print("Analog reading "); 
-  Serial.println(reading);
- 
-  // convert the value to resistance
-  reading = (1023 / reading)  - 1;     // (1023/ADC - 1) 
-  reading = SERIESRESISTOR / reading;  // 10K / (1023/ADC - 1)
-  Serial.print("Thermistor resistance "); 
-  Serial.println(reading);
- 
+
+void loop() {
+  double raw_data = analogRead(THERMISTORPIN);
+
+  /*
+  //FOR DEBUGGING:
+  Serial.print("Analog reading: ");
+  Serial.println(raw_data);
+  */
+  double scaled_data = (1023 / raw_data) - 1;
+  double thermistor_resistance = SERIESRESISTOR / scaled_data;
+
+  double v_out = 4.9 * raw_data;
+
+  Serial.print("INFO: ");
+  Serial.println(thermistor_resistance);
+
+  float steinhart;
+  steinhart = thermistor_resistance / THERMISTORNOMINAL;
+  steinhart = log(steinhart);
+  steinhart /= BCOEFFICIENT;
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15);
+  steinhart = 1.0 / steinhart;
+  steinhart -= 273.15;
+
+  Serial.print("Temperature: ");
+  Serial.print(steinhart);
+  Serial.println(" *C");
+  
   delay(1000);
+
 }
